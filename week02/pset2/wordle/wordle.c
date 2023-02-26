229f309a814110e69f102ad18c3fc151e0c4e9fb
youngfrigo <aliguler1995@yahoo.co.uk>
2023-02-26 14:39:24 +0000

    Continued work

--- week02/pset2/wordle/wordle.c
+++ week02/pset2/wordle/wordle.c
@@ -4,43 +4,45 @@
 #include <time.h>
 
 // declare list size
-const int LISTSIZE = 1000;
+const int LISTSIZE = 10;
 
 // function prototype
 char* generate_word(int wordsize);
 void word_guess_cmp(char* word, char* guess);
 
-int main(int argc, char* argv[])
+int main(void)
 {
-    // check to see if correct CLI arguments
-    if (argc < 2)
-    {
-        printf("Usage: ./wordle wordsize");
-        return 0;
-    }
+    // // check to see if correct CLI arguments
+    // if (argc < 2)
+    // {
+    //     printf("Usage: ./wordle wordsize");
+    //     return 0;
+    // }
 
-    // convert wordsize to int
-    int wordsize = atoi (argv[1]);
+    // // convert wordsize to int
+    // int wordsize = atoi (argv[1]);
 
-    // check to see if wordsize between 5 and 8
-    if (wordsize < 5 || wordsize > 8)
-    {
-        printf("Error wordsize must be either 5, 6, 7, 8\n");
-        return 2;
-    }
+    // // // check to see if wordsize between 5 and 8
+    // if (wordsize < 5 || wordsize > 8)
+    // {
+    //     printf("Error wordsize must be either 5, 6, 7, 8\n");
+    //     return 2;
+    // }
 
+    int wordsize = 5;
     // create variable for guess
     char guess[wordsize];
 
     // take input and assign to guess
-    printf("Guess a %i letter word: ", wordsize);
-    scanf("%s", guess);
-    printf("%s\n", guess);
+    // printf("Guess a %i letter word: ", wordsize);
+    // scanf("%s", guess);
+    // printf("%s\n", guess);
 
     char* wordle = malloc(sizeof(char) * wordsize);
     wordle = generate_word(wordsize);
 
-    printf("%s", wordle);
+    // printf("%s\n", wordle);
+    free(wordle);
     
     return 0;
 }
@@ -50,7 +52,7 @@ char* generate_word(int wordsize)
 {
     // get random line number
     srand(time(NULL));
-    int random_line = rand() % 1000;
+    int random_line = rand() % LISTSIZE;
 
     // create file pointer and check to see if NULL
     FILE *ptr;
@@ -70,32 +72,13 @@ char* generate_word(int wordsize)
 
     // create word variable
     char *word = malloc(sizeof(char) * wordsize);
-
-    // variable to track line number
-    int n = 0;
-
-    // while loop to check every line
-    while (fgets(word, sizeof(word) + 1, ptr) != NULL)
+    for (int i = 0; i < LISTSIZE; i++)
     {
-        // increment line number in every iteration of while loop
-        n++;
-
-        // check to see if random line number is the same
-        // print word read from file and line number
-        if (n == random_line)
-        {
-            printf("%i\n", random_line);
-        }
-
-        // once end of file is reached, break the loop
-        if (fgets(word, sizeof(word) + 1, ptr) == NULL)
-        {
-            break;
-        }
+        fgets(word, wordsize + 1, ptr);
+        printf("%s", word);
     }
     fclose(ptr);
     return word;
-    free (word);
 }
 
 void word_guess_cmp(char* word, char* guess)
