68caea1ab79f9fce8db5d8220e5c2bf5dec556aa
youngfrigo <aliguler1995@yahoo.co.uk>
2023-03-29 01:43:26 +0100

    Completed runoff file

--- week03/pset3/runoff/runoff.c
+++ week03/pset3/runoff/runoff.c
@@ -6,24 +6,14 @@
 
 // maximum number of candidates
 #define MAX 9
+#define MAX_VOTERS 100
 
 // max number of characters
 int n = 256;
 
-// number of candidates
-int cand_num;
-
-// voter preferences
-int voterChoices = 3;
-
-// preference data type
-typedef struct
-{
-    char *first;
-    char *second;
-    char *third;
-}
-votes;
+// number of candidates and voters
+int candNum;
+int numOfVoters;
 
 // candidate data type
 typedef struct
@@ -36,19 +26,20 @@ candidate;
 
 // array to hold candidates
 candidate candidates[MAX];
+int preferences[MAX_VOTERS][MAX];
 
-// function prototype
-void getVotes(int numOfVoters, votes voters[]);
-void printVotes(votes voters[]);
+// function prototypes
+int checkNames(char *name, candidate candidates[]);
+void getVotes(void);
+void tabulateVotes(void);
+bool printWinner(void);
+int findMin(void);
+void eliminate(int min);
+bool isTie(int min);
 
 int main(int argc, char *argv[])
 {
-    // choices variables
-    char *first = malloc(sizeof(char) * n);
-    char *second = malloc(sizeof(char) * n);
-    char *third = malloc(sizeof(char) * n);
-
-    cand_num = argc - 1;
+    candNum = argc - 1;
 
     // error checking commads
     if (argc < 2)
@@ -82,82 +73,183 @@ int main(int argc, char *argv[])
         candidates[i - 1].name = malloc(sizeof(char) * n);
         candidates[i - 1].name = argv[i];
         candidates[i - 1].votes = 0;
+        candidates[i - 1].eliminated = false;
     }
     
     // prompt user for the number of voters
-    int numOfVoters;
     printf("Number of voters: ");
     scanf("%i", &numOfVoters);
 
-    votes voters[numOfVoters];
+    // function call to prompt and save votes
+    getVotes();
 
-    getVotes(numOfVoters, voters);
+    while (true)
+    {
+        // calculate all votes
+        tabulateVotes();
 
-    printf("%s\n", candidates[0].name);
-    printf("%s\n", candidates[1].name);
-    printf("%s\n", candidates[2].name);
+        // if a winner has been found, stop the loop
+        bool win = printWinner();
+        if(win)
+        {
+            break;
+        }
 
-    printVotes(voters);
+        // calculate the lowest amount of votes
+        int min = findMin();
+
+        // check to see if there is a tie between un-eliminated candidates
+        bool tie = isTie(min);
+        if (tie)
+        {
+            for (int i = 0; i < candNum; i++)
+            {
+                if (candidates[i].eliminated == false)
+                {
+                    printf("%s\n", candidates[i].name);
+                }
+            }
+            break;
+        }
+
+        // eliminate any candidates with the current lowest amount of votes
+        eliminate(min);
+
+        // reset all votes back to zero
+        for (int i = 0; i < candNum; i++)
+        {
+            candidates[i].votes = 0;
+        }
+    }
 
 }
 
-void getVotes(int numOfVoters, votes voters[])
+// function definition
+int checkNames(char *name, candidate candidates[])
 {
+    // iterate through cast vote and candidates to compare
+    for (int i = 0; i < candNum; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0 && candidates[i].eliminated == false)
+        {
+            return i;
+        }
+    }
+    return 404;
+}
+
+// function definition
+void getVotes(void)
+{
+    // loop to ask number each voter for their preferences
     for (int i = 0; i < numOfVoters; i++)
     {
-        // prepare user to enter preferences
-        printf("Voter %i Ranking\n", i + 1);
-
-        // variable to hold vote
-        char *castVote = malloc(sizeof(char) * n);
-
-        // loop to iterate over preferences
-        for (int j = 0; j < voterChoices; j++)
+        // iterate through the number of candidates to ensure all preferences are taken
+        for (int j = 0; j < candNum; j++)
         {
-            // variables to ensure 
-            int matches = 0;
+            // variable to hold current vote
+            char *castVote = malloc(sizeof(char) * n);
+
             printf("Vote %i: ", j + 1);
             scanf("%s", castVote);
-            printf("%s\n", castVote);
-            for (int k = 0; k < cand_num; k++)
-            {
-                if (strcmp(candidates[k].name, castVote) == 0)
-                {
-                    if (j == 0)
-                    {
-                        voters[i].first = castVote;
-                        printf("Voter 1 - Choice 1: %s\n", voters[i].first);
-                        matches++;
-                    }
-                    else if (j == 1)
-                    {
-                        voters[i].second = castVote;
-                        printf("Voter 1 - Choice 2: %s\n", voters[i].second);
-                        matches++;
-                    }
-                    else if (j == 2)
-                    {
-                        voters[i].third = castVote;
-                        printf("Voter 1 - Choice 3: %s\n", voters[i].third);
-                        matches++;
-                    }
-                }
-            }
-            if (matches == 0)
+
+            // function to ensure name matches a candidate
+            if (checkNames(castVote, candidates) == 404)
             {
                 printf("Invalid vote.\n");
                 j--;
+                continue;
+            }
+            // if the name matches, take the first preference of that voter and increase their votes by
+            else
+            {
+                // first preference of each voter is
+                int pos = checkNames(castVote, candidates);
+                preferences[i][j] = pos;
             }
         }
     }
 }
 
-void printVotes(votes voters[])
+// function definition
+bool printWinner()
 {
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < candNum; i++)
     {
-        printf("Voter 1 - Choice 1: %s\n", voters[i].first);
-        printf("Voter 1 - Choice 2: %s\n", voters[i].second);
-        printf("Voter 1 - Choice 3: %s\n", voters[i].third);
+        if (candidates[i].votes > numOfVoters / 2)
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
     }
+    return false;
+}
+
+// function definition
+void tabulateVotes()
+{
+    // iterate for each voter
+    for (int i = 0; i < numOfVoters; i++)
+    {
+        for (int j = 0; j < candNum; j++)
+        {
+            // position of preference
+            int pos = preferences[i][j];
+
+            // increment the vote of the first choice for each voter
+            if (candidates[pos].eliminated == false)
+            {
+                candidates[pos].votes++;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
+
+// function definition
+int findMin(void)
+{
+    int min = numOfVoters / 2;
+    for (int i = 0; i < candNum; i++)
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes < min)
+        {
+            min = candidates[i].votes;
+        }
+    }
+    return min;
+}
+
+// function definition
+void eliminate(int min)
+{
+    for (int i = 0; i < candNum; i++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
+}
+
+// function definition
+bool isTie(int min)
+{
+    bool tie = false;
+    for (int i = 0; i < candNum; i++)
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes == min)
+        {
+            tie = true;
+        }
+        else if (candidates[i].eliminated == false && candidates[i].votes != min)
+        {
+            tie = false;
+        }
+    }
+    return tie;
 }
